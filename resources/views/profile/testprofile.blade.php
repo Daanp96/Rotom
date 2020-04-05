e30f1fb11e3b3e622d7266d81e86f52670a5463e
daan96 <s1115218@student.hsleiden.nl>
2020-04-05 20:18:25 +0200

    Overbodige ringtone map verwijderd

--- resources/views/profile/testprofile.blade.php
+++ resources/views/profile/testprofile.blade.php
@@ -13,6 +13,7 @@
           <div class="profile__main">
               <form action="/testprofile/store" method="post">
                   @csrf
+                  <!-- <input class="add__name" type="file" name="profile"> -->
                   <div class="profile__main__name">
                       <input type="text" name="name" class="profile__main__name__form">
                       <label for="name" class="profile__main__name__label">Naam</label>
@@ -24,7 +25,7 @@
                               <option value="none" selected>None</option>
                               <option value="custom">Custom</option>
                           </select>
-  {{--                        <p class="profile__main__options__option__select">custom</p>--}}
+  <!--                        <p class="profile__main__options__option__select">custom</p> -->
                       </div>
                       <div class="profile__main__options__option">
                           <label for="ringtone" class="profile__main__options__option__text">Ringtone</label>
@@ -40,7 +41,7 @@
                               <option value="0" selected>Uit</option>
                               <option value="1">Aan</option>
                           </select>
-  {{--                        <p class="profile__main__options__option__select">uit</p>--}}
+  <!--                      <p class="profile__main__options__option__select">uit</p> -->
                       </div>
                   </div>
                   <button type="submit" name="button">Maak contact aan</button>
