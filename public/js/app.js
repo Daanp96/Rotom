4d30518effbc38caadb1e4da4c7daf48ebb5a76b
Kim <s1114322@student.hsleiden.nl>
2020-04-16 06:40:23 -0700

    js doet het weer

--- public/js/app.js
+++ public/js/app.js
@@ -49654,42 +49654,37 @@ __webpack_require__.r(__webpack_exports__);
 
 window.onload = function () {
   //NOTIFICATIE POPUP VOOR OP ELKE PAGINA
-  var notification = document.getElementById("js--notification");
-  var notification_background = document.getElementsByClassName("notificationBox__background")[0];
-  var notification_title = document.getElementById("js--notification_title");
-  var notification_close = document.getElementById("js--notification_close");
-  var notification_text = document.getElementById("js--notification_text");
-  var notification_confirm = document.getElementById("js--notification_ok");
-
-  var open_notification = function open_notification(title, text, button_text) {
-    notification_title.innerHTML = title;
-    notification_text.innerHTML = text;
-    notification_confirm.innerHTML = button_text;
-    notification.style.display = "block";
-  };
-
-  notification_close.onclick = function (event) {
-    notification.style.display = "none";
-  };
-
-  notification_background.onclick = function (event) {
-    notification.style.display = "none";
-  }; //temp NOTIIFCATIE TEST
-
-
-  var header = document.getElementById("js--header");
-  var temp_values = {
-    title: "NOTIFICATIE",
-    text: "Er is iets fout met de bel! Er is geen vingerafdruk gescanned. Het is mogelijk dat de scanner vervangen moet worden.",
-    button: "begrepen"
-  };
-
-  header.onclick = function () {
-    open_notification(temp_values.title, temp_values.text, temp_values.button);
-  }; //---------------------
+  // const notification = document.getElementById("js--notification");
+  // const notification_background = document.getElementsByClassName("notificationBox__background")[0];
+  // const notification_title = document.getElementById("js--notification_title");
+  // const notification_close = document.getElementById("js--notification_close");
+  // const notification_text = document.getElementById("js--notification_text");
+  // const notification_confirm = document.getElementById("js--notification_ok");
+  //
+  // const open_notification = (title, text, button_text) =>{
+  //   notification_title.innerHTML = title;
+  //   notification_text.innerHTML = text;
+  //   notification_confirm.innerHTML = button_text;
+  // 
+  //   notification.style.display = "block";
+  // };
+  //
+  // notification_close.onclick = (event) =>{
+  //   notification.style.display = "none";
+  // };
+  // notification_background.onclick = (event) =>{
+  //   notification.style.display = "none";
+  // };
+  //
+  //
+  // //temp NOTIIFCATIE TEST
+  // const header = document.getElementById("js--header");
+  // const temp_values = {title:"NOTIFICATIE", text:"Er is iets fout met de bel! Er is geen vingerafdruk gescanned. Het is mogelijk dat de scanner vervangen moet worden.", button:"begrepen"};
+  // header.onclick = () =>{
+  //   open_notification(temp_values.title,temp_values.text,temp_values.button);
+  // };
+  //---------------------
   //VOLUME SLIDER FUNCTIE
-
-
   $(function () {
     if ($('input').is('.settings__slider')) {
       console.log("yeet"); //SLIDER
