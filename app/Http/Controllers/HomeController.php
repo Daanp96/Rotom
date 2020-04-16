9adcd17681a6a9a04c91d1a9e0a04eb855e5ded2
daan96 <s1115218@student.hsleiden.nl>
2020-04-16 13:12:49 +0200

    Button model + button data meegegeven aan home pagina.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Buttons;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home')->with('button', Buttons::where('button_id', "=", 2)->first());
+    }
+
+    public function opendoor(){
+
     }
 }
