67667b20b5c2f4d775816cfcf2e7d350d4478ee8
mirjamstrating <s1100440@student.hsleiden.nl>
2020-06-07 18:03:20 +0200

    bla

--- resources/js/main.js
+++ resources/js/main.js
@@ -102,13 +102,13 @@ window.onload = () =>{
     var searchbar = document.getElementById("js--searchbar");
     var listItems = document.getElementsByClassName("list history");
 
-    searchbar.onkeyup = function(event){
+    searchbar.onkeydown = function(event){
       search(this.value);
     }
     function search(filter){
       for (var i = 0; i < listItems.length; i++) {
         if (listItems[i].innerHTML.toLowerCase().indexOf(filter.toLowerCase()) !== -1) {
-          listItems[i].style.display = " ";
+          listItems[i].style.display = "";
 
         }else {
           listItems[i].style.display = "none";
